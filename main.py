366f20e303ea4bfb6160175e4d75aeff42a57dd2
Athulsudin <athulathulsudin6@gmail.com>
2026-08-04 18:53:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 # ==============================================================================
 # CONFIGURATION & MONGO DB SETUP
 # ==============================================================================
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN_HERE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "").strip()
 SUPPORT_USERNAME = "@Athulsudin"
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456789))
 
